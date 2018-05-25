147c836205d85ca7dd4f7f6daaf7a3e6df8fbb86
Noelia Sabando <lolia_15@hotmail.com>
2018-05-25 13:50:57 -0400

    proyecto con html, css y js codificado

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-/* Acá va tu código */
+function encodeButton(){
+    const offset= parseInt(document.getElementById("caja").value);
+    const message= document.getElementById("caja2").value;
+    let mensajeFinal= window.cipher.encode(offset, message);
+    document.getElementById("RespuestaCodificada").innerHTML= mensajeFinal;        
+}
+
+
+
