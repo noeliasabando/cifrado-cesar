f714fb9be5bae2c2bcfb99b8d59f39919ae15ad6
Noelia Sabando <lolia_15@hotmail.com>
2018-05-27 02:16:05 -0400

    cambie caja de ingresar mensaje y arreglos en css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function encodeButton(){
     const offset= parseInt(document.getElementById("caja").value);
     const message= document.getElementById("caja2").value;
     let mensajeFinal= window.cipher.encode(offset, message);
-    document.getElementById("RespuestaCodificada").innerHTML= mensajeFinal;        
+    document.getElementById("RespuestaCodificada").innerHTML= mensajeFinal;       
 }
 
 function decodeButton(){
