05540a29a7ce90a052a46c54e50a4615c8ff323d
Noelia Sabando <lolia_15@hotmail.com>
2018-07-28 22:35:27 -0400

    cambio diseño

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function encodeButton(){
     const offset= parseInt(document.getElementById("caja").value);
-    const message= document.getElementById("caja2").value;
+    const message= document.getElementById("inputText").value;
     let mensajeFinal= window.cipher.encode(offset, message);
     document.getElementById("RespuestaCodificada").innerHTML= mensajeFinal;       
 }
 
 function decodeButton(){
     const offset= parseInt(document.getElementById("caja").value);
-    const message= document.getElementById("caja2").value;
+    const message= document.getElementById("inputText").value;
     let mensajeFinal= window.cipher.decode(offset, message);
     document.getElementById("RespuestaDecodificada").innerHTML= mensajeFinal;
 }
